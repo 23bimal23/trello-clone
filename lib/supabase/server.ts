f62682061f40005421469cb21bcaf8a6534590ff
23bimal23 <bimal23paudel777@gmail.com>
2025-07-25 22:15:20 +0545

    supabase integration completed to create board with default coli=umns

---
+++ lib/supabase/server.ts
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export async function createClient() {
+    const cookieStore = await cookies();
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANNON_KEY!;
+
+    return createServerClient(supabaseUrl, supabaseAnonKey, {
+        cookies: {
+            getAll() {
+                return cookieStore.getAll()
+            },
+            setAll(cookiesToSet) {
+                try {
+                    cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+                }catch (error) {
+                    console.error("Error setting cookies:", error);
+                }
+           }
+        },
+    });
+}
